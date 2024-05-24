1b715c926f5441f517e0005851299d03c1b51628
Gabriel Oliveira de Lima Rodrigues <g.de.lima.rodrigues@avanade.com>
2024-05-24 15:25:22 -0300

    Criação do componente Comment para comentários

---
+++ src/components/Comment.jsx
@@ -0,0 +1,38 @@
+import { ThumbsUp, Trash } from 'phosphor-react';
+import styles from './Comment.module.css';
+
+export function Comment() {
+    return (
+        <div className={styles.comment}>
+            <img src="https://github.com/gabrielderodrigues.png" />
+            
+            <div className={styles.commentBox}>
+                <div className={styles.commentContent}>
+                    <header>
+                        <div className={styles.authorAndTime}>
+                            <strong>Gabriel Oliveira</strong>
+                            <time 
+                                title="24 de maio às 12:02"
+                                dateTime="2024-05-24 12:03:10">
+                                    Cerca de 1h atrás
+                            </time>
+                        </div>
+
+                        <button title="Deletar comentário">
+                            <Trash size={20} />
+                        </button>
+                    </header>
+
+                    <p>
+                        Muito bom, Devon! Parabéns!
+                    </p>
+                </div>
+
+                <footer>
+                    <ThumbsUp />
+                    Aplaudir <span>20</span>
+                </footer>
+            </div>
+        </div>
+    );
+}
