55abe2d59eac9b2048aa82207c98ec50c5b23c4d
Gabriel Oliveira de Lima Rodrigues <g.de.lima.rodrigues@avanade.com>
2024-05-24 16:45:54 -0300

    Estilização dos comentários

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -19,7 +19,7 @@ export function Comment() {
                         </div>
 
                         <button title="Deletar comentário">
-                            <Trash size={20} />
+                            <Trash size={24} />
                         </button>
                     </header>
 
@@ -29,8 +29,10 @@ export function Comment() {
                 </div>
 
                 <footer>
-                    <ThumbsUp />
-                    Aplaudir <span>20</span>
+                    <button>
+                        <ThumbsUp />
+                        Aplaudir <span>20</span>
+                    </button>
                 </footer>
             </div>
         </div>
