de501dca3132f0647d9db85241183689ae928081
Gabriel Oliveira de Lima Rodrigues <g.de.lima.rodrigues@avanade.com>
2024-05-24 10:52:48 -0300

    Criação dos componentes Post e Sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,28 @@
 import { Header } from './components/Header';
+import { Post } from './Post';
+
+import styles from './App.module.css';
 import './global.css';
+import { Sidebar } from './components/Sidebar';
 
 export function App() {
   return (
     <div>
       <Header />
-      <p>Olá</p>
+      
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post 
+            author="Gabriel Oliveira"
+            content="Lorem ipsum dolor sit amet consectur adipiscing elit."
+          />
+          <Post 
+            author="Vivian Fortuna"
+            content="Lorem ipsum dolor sit amet consectur adipiscing elit."
+          />
+        </main>
+      </div>
     </div>
   );
 }
