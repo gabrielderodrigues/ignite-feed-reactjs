32663314b5e56e9f6f87763622525a75234e4700
Gabriel Oliveira de Lima Rodrigues <g.de.lima.rodrigues@avanade.com>
2024-05-24 12:05:46 -0300

    Criação do componente Post

---
+++ src/components/Post.jsx
@@ -0,0 +1,34 @@
+import styles from './Post.module.css';
+
+export function Post() {
+    return (
+        <article className={styles.post}>
+            <header>
+                <div className={styles.author}>
+                    <img 
+                        className={styles.avatar}
+                        src="https://avatars.githubusercontent.com/u/56936740?v=4"
+                    />
+
+                    <div className={styles.authorInfo}>
+                        <strong>Gabriel Oliveira</strong>
+                        <span>Web Developer</span>
+                    </div>
+                </div>
+
+                <time 
+                    title="24 de maio às 12:02"
+                    dateTime="2024-05-24 12:03:10">
+                        Publico há 1h
+                </time>
+            </header>
+
+            <div className={styles.content}>
+                <p>Fala galeraa 👋</p>
+                <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
+                <p>👉 <a href="">jane.design/doctorcare</a></p>
+                <p><a href="">#novoprojeto #nlw #rocketseat</a></p>                    
+            </div>
+        </article>
+    );
+}
